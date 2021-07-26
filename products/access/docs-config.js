6bc3373f8ef7e64a65acfbd28038c42c8a03281b
0racl3z <77244572+0racl3z@users.noreply.github.com>
2021-07-26 12:17:36 -0700

    Initialize teams-structural-changes

---
+++ products/access/docs-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  product: "Access",
+  pathPrefix: "/access",
+  productIconKey: "access",
+  contentRepo: "cloudflare/cloudflare-docs",
+  contentRepoFolder: "products/access",
+  externalLinks: [
+    {
+      title: "Cloudflare for Teams: Access home",
+      url: "https://www.cloudflare.com/teams/access/"
+    },
+    {
+      title: "Cloudflare for Teams home",
+      url: "https://www.cloudflare.com/teams/"
+    },
+    {
+      title: "Cloudflare for Teams pricing",
+      url: "https://www.cloudflare.com/teams-pricing/"
+    }
+  ],
+  search: {
+    indexName: "cloudflare-access",
+    apiKey: "b3c13d590b79cf5f74f8ebc0d58fe73a"
+  },
+  siteMetadata: {
+    title: "Cloudflare Access docs",
+    description: "Welcome to Cloudflare Access. You can now make all your applications available on the internet without a VPN. Access protects these applications and allows only authorized users to access them. For example, Cloudflare uses Access to ensure only people at Cloudflare can access internal tools like our staging site.",
+    author: "@cloudflare",
+    url: "https://developers.cloudflare.com/access",
+    image: "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="
+  }
+}
---
+++ products/cloudflare-one/src/content/index.md
@@ -0,0 +1,26 @@
+---
+title: Home
+order: 0
+type: overview
+---
+
+<ContentColumn>
+
+# Cloudflare for Teams documentation
+
+Your employees, partners, and customers need a network that is secure, fast and reliable to get work done. Cloudflare for Teams replaces legacy security perimeters with our global edge, making the Internet faster and safer for teams around the world.
+
+Cloudflare for Teams provides seamless access to any application and the Internet from any device, anywhere. 
+
+<TableWrap>
+
+|Product|Description|Documentation|
+|---|---|---|
+|**Access**|Zero Trust security for self-hosted and SaaS applications|[Link](https://developers.cloudflare.com/access/)|
+|**Gateway**|Secure Web Gateway filtering and connectivity|[Link](https://developers.cloudflare.com/gateway/)|
+|**Argo Tunnel**|Outbound-only connections to Cloudflare's edge|[Link](https://developers.cloudflare.com/argo-tunnel/)|
+|**WARP**|Mobile and desktop client which connects devices to Cloudflare's network|[Link](https://developers.cloudflare.com/warpclient/)|
+
+</TableWrap>
+
+</ContentColumn>
