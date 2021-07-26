06ce67d3197420e46aa20d33d3a288b25d30a39a
0racl3z <77244572+0racl3z@users.noreply.github.com>
2021-07-26 12:17:31 -0700

    Initialize teams-tutorials

---
+++ products/1.1.1.1/docs-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  product: "1.1.1.1",
+  pathPrefix: "/1.1.1.1",
+  productIconKey: "1.1.1.1",
+  contentRepo: "cloudflare/cloudflare-docs",
+  contentRepoFolder: "products/1.1.1.1",
+  externalLinks: [
+    {
+      title: "1.1.1.1 website",
+      url: "https://1.1.1.1"
+    },
+    {
+      title: "Cloudflare homepage",
+      url: "https://cloudflare.com"
+    }
+  ],
+  search: {
+    indexName: "",
+    apiKey: "",
+  },
+  siteMetadata: {
+    title: "1.1.1.1 docs",
+    description: "A blazing fast DNS resolver built for private browsing.",
+    author: "@cloudflare",
+    url: "https://developers.cloudflare.com/1.1.1.1",
+    image: "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="
+  }
+}
---
+++ products/cloudflare-one/src/content/index.md
@@ -0,0 +1,26 @@
+---
+title: Home
+order: 0
+type: overview
+---
+
+<ContentColumn>
+
+# Cloudflare for Teams documentation
+
+Your employees, partners, and customers need a network that is secure, fast and reliable to get work done. Cloudflare for Teams replaces legacy security perimeters with our global edge, making the Internet faster and safer for teams around the world.
+
+Cloudflare for Teams provides seamless access to any application and the Internet from any device, anywhere. 
+
+import Stripe from "../components/stripe.js"
+import ProductGrid from "../components/product-grid.js"
+
+<Stripe>
+
+## Products
+</Stripe>
+<TableWrap>
+
+<ProductGrid/>
+</TableWrap>
+</ContentColumn>
