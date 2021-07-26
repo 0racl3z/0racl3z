5478f4c6e49d9944cdd0637b7df6234d1bf01712
0racl3z <77244572+0racl3z@users.noreply.github.com>
2021-07-26 12:17:52 -0700

    Initialize abracchi-tw-patch-1

---
+++ products/gateway/docs-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  product: "Gateway",
+  pathPrefix: "/gateway",
+  productIconKey: "gateway",
+  contentRepo: "cloudflare/cloudflare-docs",
+  contentRepoFolder: "products/gateway",
+  externalLinks: [
+    {
+      title: "Cloudflare homepage",
+      url: "https://cloudflare.com"
+    }
+  ],
+  search: {
+    indexName: "",
+    apiKey: "",
+  },
+  siteMetadata: {
+    title: "Cloudflare Gateway docs",
+    description: "Cloudflare Gateway protects enterprises, their devices, their networks and their data by securing every connection without compromising performance.",
+    author: "@cloudflare",
+    url: "https://developers.cloudflare.com/gateway",
+    image: "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="
+  }
+}
---
+++ products/workers/docs-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  product: "Workers",
+  pathPrefix: "/workers",
+  productIconKey: "workers",
+  contentRepo: "cloudflare/cloudflare-docs",
+  contentRepoFolder: "products/workers",
+  externalLinks: [
+    {
+      title: "Workers home",
+      url: "https://workers.cloudflare.com"
+    },
+    {
+      title: "Playground",
+      url: "https://cloudflareworkers.com"
+    },
+    {
+      title: "Pricing",
+      url: "https://workers.cloudflare.com/#plans"
+    },
+  ],
+  search: {
+    indexName: "cloudflare-workers-v2",
+    apiKey: "9c24e56570a37c30bba608dad543d1d8",
+  },
+  siteMetadata: {
+    title: "Cloudflare Workers docs",
+    description: "Documentation for Cloudflare Workers, a serverless execution environment that allows you to create entirely new applications or augment existing ones without configuring or maintaining infrastructure.",
+    author: "@cloudflare",
+    url: "http://developers.cloudflare.com/workers",
+    image: "http://developers.cloudflare.com/workers/og-image.png",
+  }
+}
