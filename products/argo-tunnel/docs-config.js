c315c1ea4be9cde1e9cd03e40fdd482e4411429c
0racl3z <77244572+0racl3z@users.noreply.github.com>
2021-07-26 12:17:59 -0700

    Initialize 2020.12.08-teams-tutorials

---
+++ products/argo-tunnel/docs-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  product: "Argo Tunnel",
+  pathPrefix: "/argo-tunnel",
+  productIconKey: "argo-tunnel",
+  contentRepo: "cloudflare/cloudflare-docs",
+  contentRepoFolder: "products/argo-tunnel",
+  externalLinks: [
+    {
+      title: "Argo Tunnel home",
+      url: "https://www.cloudflare.com/products/argo-tunnel/"
+    },
+    {
+      title: "cloudflared on GitHub",
+      url: "https://github.com/cloudflare/cloudflared"
+    }
+  ],
+  search: {
+    indexName: "",
+    apiKey: "",
+  },
+  siteMetadata: {
+    title: "Cloudflare Argo Tunnel docs",
+    description: "Argo Tunnel exposes applications running on your local web server, on any network with an Internet connection, without adding DNS records or configuring a firewall or router. It just works.",
+    author: "@cloudflare",
+    url: "https://developers.cloudflare.com/argo-tunnel",
+    image: "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="
+  }
+}
